2ea44fa32df246d0ded409fed623645e7c87df62
Uenderss <meajudabarto@gmail.com>
2022-09-19 17:03:07 -0300

    upgrade

--- src/utils/typeUtils.ts
+++ src/utils/typeUtils.ts
@@ -1,4 +1,4 @@
-import { Users,Test } from "@prisma/client";
+import { Users,Test,TeachersDiscipline, Discipline, Teachers } from "@prisma/client";
 
 
 export type TCreateUserData = Omit<Users, "id">;
@@ -6,3 +6,4 @@ export type TCreateTestData = Omit<Test, "id" | "teacherDisciplineId" | "view" >
     teacherId: number;
     disciplineId: number;
   };
+export type TTeachersDiscipline ={teacher:Teachers,teacherId:number, discipline:Discipline,disciplineId:number, categoryId:number,tests:Test[]}
