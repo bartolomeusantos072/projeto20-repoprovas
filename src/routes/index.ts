3e20f7aff7935fb06ce96f41fedaf3b41dbc660d
Uenderss <meajudabarto@gmail.com>
2022-09-19 07:41:36 -0300

    implementacao feature discipline

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,13 +1,14 @@
 import { Router } from "express";
 import userRouter from "./userRouter";
 import teacherRouter from "./teacherRouter";
+import disciplineRouter from "./disciplineRouter";
 // import testRouter from "./teacherRouter";
 
 const router = Router();
 
 router.use(userRouter);
 router.use(teacherRouter);
-
+router.use(disciplineRouter);
 // router.use(testRouter);
 
 export default router;
