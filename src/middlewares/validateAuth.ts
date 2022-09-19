84eb806f8637fecd4ae12da51a43c2f7a0642b31
Uenderss <meajudabarto@gmail.com>
2022-09-19 00:20:20 -0300

    create validate token

---
+++ src/middlewares/validateAuth.ts
@@ -0,0 +1,27 @@
+import { NextFunction, Request, Response } from "express";
+import jwt from "jsonwebtoken";
+import userService from "../services/userService";
+import { unauthorizedError } from "../utils/errorUtils";
+import dotenv from "dotenv";
+dotenv.config();
+
+export async function validateAuth(req: Request, res: Response, next: NextFunction) {
+
+    const authorization = req.headers["authorization"];
+    if (!authorization) throw unauthorizedError("Authorization not found");
+
+    const token = authorization.split(" ")[1].trim();
+    if (!token) throw unauthorizedError("Token not found");
+
+    try {
+        const { userId } = jwt.verify(token, process.env.JWT_SECRET) as {
+            userId: number;
+        };
+        const user = await userService.findId(userId);
+        res.locals.user = user;
+
+        next();
+    } catch {
+        throw unauthorizedError("Invalid token");
+    }
+}
